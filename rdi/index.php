3d05d16a63806601e6ff3431876a21409260b822
Rafael Dutra Figueira Batista <128432639+Rayengsof@users.noreply.github.com>
2025-02-13 17:37:41 -0300

    Update index.php

--- rdi/index.php
+++ rdi/index.php
@@ -59,7 +59,7 @@ if (isset($_POST['action'])) {
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=0">
     <title>RDI Osasco</title>
     <link rel="stylesheet" href="CSS/styles.css">
     <link rel="stylesheet" href="CSS/chat.css">
